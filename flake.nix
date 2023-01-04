5b57cf620d63d5fffa9bdfa355b8d71031b22218
Alaina <68250402+alaidriel@users.noreply.github.com>
2023-01-04 11:29:28 -0600

    ci: build packages (#1)

--- flake.nix
+++ flake.nix
@@ -3,14 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    overlays.default = final: prev:
-      let pkgs = prev; in {
-        etherea = pkgs.callPackage ./pkgs/etherea.nix { inherit pkgs; };
-        athena = pkgs.callPackage ./pkgs/athena.nix { inherit pkgs; };
-      };
-  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system: {
+        packages =
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          in
+          {
+            etherea = pkgs.callPackage ./pkgs/etherea.nix { inherit pkgs; };
+            athena = pkgs.callPackage ./pkgs/athena.nix { inherit pkgs; };
+          };
+      }) // {
+      overlays.default = final: prev:
+        let pkgs = prev; in {
+          etherea = pkgs.callPackage ./pkgs/etherea.nix { inherit pkgs; };
+          athena = pkgs.callPackage ./pkgs/athena.nix { inherit pkgs; };
+        };
+    };
 }
