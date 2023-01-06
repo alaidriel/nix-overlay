0ae9d4425add4cc78f55a41b4921128c3f5bbfa1
Alaina <68250402+alaidriel@users.noreply.github.com>
2023-01-06 12:06:08 -0600

    feat: update zig

--- pkgs/zig-custom.nix
+++ pkgs/zig-custom.nix
@@ -1,22 +1,8 @@
 { pkgs, lib, stdenv, ... }:
 
 let
-  platform =
-    let
-      os =
-        if stdenv.isLinux
-        then "linux"
-        else "macos";
-      arch =
-        if stdenv.isx86_64
-        then "x86_64"
-        else "aarch64";
-    in
-    "${os}-${arch}";
-  sha256 = {
-    macos-aarch64 = "0a16ff888bbf11ac7752847c100270be5e9c19927b539a92ba33e45004308ffe";
-    macos-x86_64 = "87f32ad9810e161b7691bf0b1968fd0a4289bd4757f87243468ed4c633d6da3f";
-  };
+  arch = "x86_64";
+  sha256 = "87f32ad9810e161b7691bf0b1968fd0a4289bd4757f87243468ed4c633d6da3f";
   version = "0.11.0-dev.1201+301a89849";
 in
 stdenv.mkDerivation {
@@ -25,8 +11,8 @@ stdenv.mkDerivation {
   pname = "zig";
 
   src = builtins.fetchurl {
-    url = "https://ziglang.org/builds/zig-${platform}-${version}.tar.xz";
-    sha256 = "sha256:${sha256.${platform}}";
+    inherit sha256;
+    url = "https://ziglang.org/builds/zig-macos-${arch}-${version}.tar.xz";
   };
 
   installPhase = ''
@@ -36,7 +22,7 @@ stdenv.mkDerivation {
     cp -r doc $out/doc
   '';
 
-  meta = with lib; {
-    platforms = platforms.darwin;
+  meta = {
+    platforms = [ "${arch}-darwin" ];
   };
 }
