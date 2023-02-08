9780e50484c6ab4e35b3b4765fc3789120d104c9
Alaina <68250402+alaidriel@users.noreply.github.com>
2023-02-07 19:26:04 -0600

    chore: update zig version

--- pkgs/zig-custom.nix
+++ pkgs/zig-custom.nix
@@ -5,8 +5,8 @@
   ...
 }: let
   arch = "x86_64";
-  sha256 = "87f32ad9810e161b7691bf0b1968fd0a4289bd4757f87243468ed4c633d6da3f";
-  version = "0.11.0-dev.1201+301a89849";
+  sha256 = "14wi1il7rirl2097m2sywslnkn1sysx2gfj69zs6wk1s4n1jjn8f";
+  version = "0.11.0-dev.1580+a5b34a61a";
 in
   stdenv.mkDerivation {
     inherit version;
