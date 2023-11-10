95d11bb1ec8b51294e3677e60893df84cb25d650
Alaina <68250402+alaidriel@users.noreply.github.com>
2023-11-09 21:20:24 -0600

    Switch x86 to arm

--- pkgs/zig-custom.nix
+++ pkgs/zig-custom.nix
@@ -4,7 +4,7 @@
   stdenv,
   ...
 }: let
-  arch = "aarch64_darwin";
+  arch = "aarch64";
   sha256 = "14wi1il7rirl2097m2sywslnkn1sysx2gfj69zs6wk1s4n1jjn8f";
   version = "0.11.0-dev.1580+a5b34a61a";
 in
