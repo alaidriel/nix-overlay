d0044f3c497f75578b8d225108f6e4a4070daad8
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-01-05 21:23:13 -0600

    use upstream instead of fork

--- pkgs/folderify.nix
+++ pkgs/folderify.nix
@@ -11,9 +11,9 @@ rustPlatform.buildRustPackage rec {
   version = "3.0.13";
 
   src = fetchFromGitHub {
-    owner = "alaidriel";
+    owner = "lgarron";
     repo = pname;
-    rev = "ecba53e7997121470b245b3cd514e2a8697a992c";
+    rev = "23c336a3cb798824b0e060757cc62bf748a16939";
     sha256 = "KgZr6vnKG5+tXHWlgThPDtu6ZesXUUPpeGz+AqNgT6c=";
   };
 
