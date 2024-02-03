2cd61d213f14e20e47a2a59f9ce97855049964f7
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-02-03 08:38:10 -0600

    Update zig version

--- pkgs/zig-custom.nix
+++ pkgs/zig-custom.nix
@@ -6,7 +6,7 @@
 }: let
   arch = "aarch64";
   sha256 = "14wi1il7rirl2097m2sywslnkn1sysx2gfj69zs6wk1s4n1jjn8f";
-  version = "0.11.0-dev.1580+a5b34a61a";
+  version = "0.12.0-dev.2543+9eda6ccef";
 in
   stdenv.mkDerivation {
     inherit version;
